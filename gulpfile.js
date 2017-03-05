3ffe5c6392f154349f0453c9e1518faf8120cd2b
Aman Pandey <afficiona4code@gmail.com>
2017-03-05 13:33:50 +0530

    Setup index html as entry point

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ var paths = {
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
-            baseDir: paths.build
+            baseDir: ''
         }
     });
 });
@@ -51,22 +51,16 @@ gulp.task('scripts', function() {
 
 gulp.task('copy:html', function () {
     return gulp.src(paths.app + '/index.html')
-        .pipe(gulp.dest(paths.build))
+        .pipe(gulp.dest(''))
         .pipe(browserSync.stream());
 });
 
 gulp.task('copy:assets', function () {
     return gulp.src(paths.app + '/assets/**/*')
-        .pipe(gulp.dest('build/assets'));
-});
-
-// Rerun the task when a file changes
-gulp.task('watch', function() {
-    gulp.watch(paths.scripts, ['scripts']);
-    gulp.watch(paths.styles, ['sass']);
-    gulp.watch(paths.app + '/index.html', ['copy:html']);
+        .pipe(gulp.dest('build/assets'))
+        .pipe(browserSync.stream());
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'scripts', 'browser-sync', 'copy:html', 'copy:assets']);
+gulp.task('default', ['sass', 'scripts', 'copy:html', 'copy:assets', 'browser-sync']);
 gulp.task('build', ['sass', 'scripts', 'copy:html']);
