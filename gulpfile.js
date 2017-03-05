f5739a84211aa09b78b39214d52392512cdedde2
Aman Pandey <afficiona4code@gmail.com>
2017-03-05 13:04:02 +0530

    Setup bootstrap for the Project

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var del = require('del');
+var browserSync = require('browser-sync').create();
+
+var paths = {
+    app: 'app',
+    build: 'build',
+    scripts: ['app/scripts/**/*.js', '!node_modules', '!bower_modules'],
+    styles: ['app/styles/**/*.scss', '!node_modules', '!bower_modules']
+};
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: paths.build
+        }
+    });
+});
+
+// Static server
+gulp.task('show-demo', function() {
+    browserSync.init({
+        server: {
+            baseDir: paths.build
+        }
+    });
+});
+
+gulp.task('sass', function () {
+    return gulp.src(paths.styles)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(paths.build + '/styles'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('scripts', function() {
+    // Minify and copy all JavaScript (except vendor scripts)
+    // with sourcemaps all the way down
+    return gulp.src(paths.scripts)
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(concat('all.min.js'))
+        .pipe(gulp.dest(paths.build + '/scripts'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('copy:html', function () {
+    return gulp.src(paths.app + '/index.html')
+        .pipe(gulp.dest(paths.build))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('copy:assets', function () {
+    return gulp.src(paths.app + '/assets/**/*')
+        .pipe(gulp.dest('build/assets'));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+    gulp.watch(paths.scripts, ['scripts']);
+    gulp.watch(paths.styles, ['sass']);
+    gulp.watch(paths.app + '/index.html', ['copy:html']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['watch', 'scripts', 'browser-sync', 'copy:html', 'copy:assets']);
+gulp.task('build', ['sass', 'scripts', 'copy:html']);
