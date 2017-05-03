8f7a070957d55b93581799126968bf00c5ae9bf6
Aman Pandey <afficiona4code@gmail.com>
2017-05-03 19:00:52 +0530

    Implement a mock of google auto-draw

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,37 +8,45 @@
 
 (function($) {
   'use strict';
-  var attemptsCounter = 0;
-  var currentAttempt = null;
-  // List of buttons who are already acted on with success
-  var invalidButtons = [];
 
-  $(document).ready(function () {
+  var defaults = {
+    clicked: false,
+    startX: 0,
+    startY: 0,
+    endX: 0,
+    endY: 0
+  };
 
-    updateAttemptsCounter(attemptsCounter);
+  $('.hero').on('mousedown', function (e) {
+    defaults.clicked = true;
+    defaults.startX = e.clientX;
+    defaults.startY = e.clientY;
+  });
 
-    $('.button').click(function () {
-      var thisButtonVal = $(this).data('val');
-
-      // Not doing anything if invalid button
-      if (invalidButtons.indexOf(thisButtonVal) !== -1) {
-        alert('invalid');
-        return;
-      };
-      if (!currentAttempt) {
-        currentAttempt = thisButtonVal;
-      } else {
-        if (currentAttempt !== thisButtonVal) {
-          updateAttemptsCounter(++attemptsCounter);
-        } else {
-          invalidButtons.push(thisButtonVal);
-        }
-        currentAttempt = null;
-      }
-    });
-
-    function updateAttemptsCounter(val) {
-      $('#attemptsCounter').html(val);
+  $('.hero').on('mousemove', function (e) {
+    var start = $('<div class="start"></div>');
+    if (defaults.clicked) {
+      start.css({'left': e.offsetX, 'top': e.offsetY});
+      $(this).append(start);
     }
+  });
+
+  $('.hero').on('mouseup', function (e) {
+    defaults.clicked = false;
+    defaults.endX = e.clientX;
+    defaults.endY = e.clientY;
+    defaults.degree = Math.atan2(defaults.endY - defaults.startY, defaults.endX - defaults.startX) * 180 / Math.PI;
+    defaults.hypotenuse = (defaults.endX - defaults.startX) / Math.cos(defaults.degree * Math.PI / 180);
+    $(this).find('.start').remove();
+    var line = $('<div class="line"></div>');
+    line.css({
+      'left': defaults.startX,
+      'top': defaults.startY,
+      'width': defaults.hypotenuse,
+      'transform': 'rotate(' + defaults.degree + 'deg)'
+    });
+    $(this).append(line);
+    defaults.startX = defaults.startY = defaults.endX = defaults.endY = defaults.degree = defaults.hypotenuse = 0;
+    console.log(defaults);
   });
 })(jQuery);
